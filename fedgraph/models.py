d1c43df8fba51b89be174053e4644bebdd677e4b
yh-yao <yh-yao@foxmail.com>
2022-09-29 21:39:36 -0400

    documents and gcn model

--- fedgraph/models.py
+++ fedgraph/models.py
@@ -1,1 +1,34 @@
 # under construction
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from torch_geometric.nn import GCNConv
+
+
+
+class GCN(torch.nn.Module):
+    def __init__(self, nfeat, nhid, nclass, dropout, NumLayers):
+        super(GCN, self).__init__()
+
+        self.convs = torch.nn.ModuleList()
+        self.convs.append(
+            GCNConv(nfeat, nhid, normalize=True, cached=True))
+        for _ in range(NumLayers - 2):
+            self.convs.append(
+                GCNConv(nhid, nhid, normalize=True, cached=True))
+        self.convs.append(
+            GCNConv(nhid, nclass, normalize=True, cached=True))
+
+        self.dropout = dropout
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+
+    def forward(self, x, adj_t):
+        for conv in self.convs[:-1]:
+            x = conv(x, adj_t)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.convs[-1](x, adj_t)
+        return torch.log_softmax(x, dim=-1)
