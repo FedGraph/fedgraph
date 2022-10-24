a5f1bd804fc13a5a8ec1c6e35509c0b50559ab8c
Jiayu Chang <jychang012@outlook.com>
2022-10-24 00:18:29 -0400

    Add setup.py, add long description

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+__version__ = ""
+
+with open("fedgraph/version.py", "r") as f:
+    exec(f.read(), globals())
+
+
+with open("README.md", "r") as f:
+    README = f.read()
+
+setup(
+    name="fedgraph",
+    version=__version__,
+    author="Jiayu Chang, Yuhang Yao",
+    author_email="jiayuc@andrew.cmu.edu, yuhangya@andrew.cmu.edu",
+    description="Federated Graph Learning",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FedGraph/fedgraph",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["Graph Neural Networks", "Federated Learning"],
+    python_requires=">=3.9",
+    install_requires=[
+        "torch-scatter>=2.0.9",
+        "torch-sparse>=0.6.15",
+        "torch-cluster>=1.6.0",
+        "torch-spline-conv>=1.2.1",
+        "torch-geometric>=2.1.0.post1",
+    ],
+    extras_require={"dev": ["build", "mypy", "pre-commit", "pytest"]},
+)
