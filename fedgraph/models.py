edcb8efb118d9717b4113d94fae2629a2e3ed1dd
Yuhang Yao <yh-yao@foxmail.com>
2022-11-10 15:24:13 -0500

    Revert "Resolved merge conflict by removing files."
    This reverts commit 636fbff593503d2e025d397fd825702947951ee3, reversing
    changes made to b3d1f716a47be6a93483eb4027abbf1cd0d8e7dc.

---
+++ fedgraph/models.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn.functional as F
+from torch import Tensor
+from torch_geometric.nn import GCNConv
+from torch_geometric.typing import Adj
+
+
+class GCN(torch.nn.Module):
+    def __init__(
+        self, nfeat: int, nhid: int, nclass: int, dropout: float, nlayers: int
+    ) -> None:
+        super(GCN, self).__init__()
+
+        self.convs = torch.nn.ModuleList()
+        self.convs.append(GCNConv(nfeat, nhid, normalize=True, cached=True))
+        for _ in range(nlayers - 2):
+            self.convs.append(GCNConv(nhid, nhid, normalize=True, cached=True))
+        self.convs.append(GCNConv(nhid, nclass, normalize=True, cached=True))
+
+        self.dropout = dropout
+
+    def reset_parameters(self) -> None:
+        for conv in self.convs:
+            conv.reset_parameters()
+
+    def forward(self, x: Tensor, adj_t: Adj) -> Tensor:
+        for conv in self.convs[:-1]:
+            x = conv(x, adj_t)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.convs[-1](x, adj_t)
+        return torch.log_softmax(x, dim=-1)
