4ee752fdfebbe9639a29e8b872286e771d4d57de
cyfan11 <74555952+cyfan11@users.noreply.github.com>
2024-01-23 16:36:14 -0500

    edit missing format

--- fedgraph/data_process.py
+++ fedgraph/data_process.py
@@ -86,13 +86,11 @@ def load_data(dataset_str: str) -> tuple:
     ----
     ind.dataset_str.x => the feature vectors of the training instances as scipy.sparse.csr.csr_matrix object;
     ind.dataset_str.tx => the feature vectors of the test instances as scipy.sparse.csr.csr_matrix object;
-    ind.dataset_str.allx => the feature vectors of both labeled and unlabeled training instances
-        (a superset of ind.dataset_str.x) as scipy.sparse.csr.csr_matrix object;
+    ind.dataset_str.allx => the feature vectors of both labeled and unlabeled training instances (a superset of ind.dataset_str.x) as scipy.sparse.csr.csr_matrix object;
     ind.dataset_str.y => the one-hot labels of the labeled training instances as numpy.ndarray object;
     ind.dataset_str.ty => the one-hot labels of the test instances as numpy.ndarray object;
     ind.dataset_str.ally => the labels for instances in ind.dataset_str.allx as numpy.ndarray object;
-    ind.dataset_str.graph => a dict in the format {index: [index_of_neighbor_nodes]} as collections.defaultdict
-        object;
+    ind.dataset_str.graph => a dict in the format {index: [index_of_neighbor_nodes]} as collections.defaultdict object;
     ind.dataset_str.test.index => the indices of test instances in graph, for the inductive setting as list object.
 
     All objects above must be saved using python pickle module.
