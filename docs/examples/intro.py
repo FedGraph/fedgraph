01761ac5f1f7fcd7adc91cbe5091df3549630ee7
Yuhang Yao <yh-yao@foxmail.com>
2024-01-24 00:00:09 -0500

    update readme and intro.py

--- docs/examples/intro.py
+++ docs/examples/intro.py
@@ -11,26 +11,11 @@ you have basic familiarity with PyTorch and PyTorch Geometric (PyG).
 
 
 import argparse
-import os
-from pathlib import Path
 from typing import Any
 
 import numpy as np
 import ray
 import torch
-import yaml
-from torch.utils.tensorboard import SummaryWriter
-
-print(os.getcwd())
-print(os.listdir())
-print(os.listdir(".."))
-
-import sys
-
-# adding additional module folders
-sys.path.append(os.path.join(sys.path[0], "src", "utility"))
-sys.path.append(os.path.join(sys.path[0], "src", "data"))
-
 
 ray.init()
 
@@ -40,9 +25,11 @@ from fedgraph.trainer_class import Trainer_General
 from fedgraph.utils import (
     get_1hop_feature_sum,
     get_in_comm_indexes,
-    increment_dir,
     label_dirichlet_partition,
 )
+
+np.random.seed(42)
+torch.manual_seed(42)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -62,14 +49,8 @@ if __name__ == "__main__":
 
     parser.add_argument("-l", "--logdir", default="./runs", type=str)
 
-    parser.add_argument("-r", "--repeat_time", default=10, type=int)
+    parser.add_argument("-r", "--repeat_time", default=1, type=int)
     args = parser.parse_args()
-    print(args)
-
-    # 'cora', 'citeseer', 'pubmed' #simulate #other dataset twitter,
-    # 'ogbn-arxiv', reddit, "ogbn-products"
-    np.random.seed(42)
-    torch.manual_seed(42)
 
     features, adj, labels, idx_train, idx_val, idx_test = load_data(args.dataset)
     class_num = labels.max().item() + 1
@@ -82,34 +63,21 @@ if __name__ == "__main__":
     row, col, edge_attr = adj.coo()
     edge_index = torch.stack([row, col], dim=0)
 
+    num_cpus_per_client = 1
     # specifying a target GPU
     if args.gpu:
         device = torch.device("cuda")
-        # running on a local machine with multiple gpu
-        if args.dataset == "ogbn-products":
-            edge_index = edge_index.to("cuda:7")
-        else:
-            edge_index = edge_index.to("cuda:0")
+        edge_index = edge_index.to("cuda:0")
+        num_gpus_per_client = 1
     else:
         device = torch.device("cpu")
-
-    if device.type == "cpu":
-        num_cpus = 0.1
-        num_gpus = 0.0
-    elif args.dataset == "ogbn-arxiv":
-        num_cpus = 5.0
-        num_gpus = 0.5
-    else:
-        num_cpus = 10
-        num_gpus = 1.0
+        num_gpus_per_client = 0
 
     # repeat experiments
     average_final_test_loss_repeats = []
     average_final_test_accuracy_repeats = []
 
     for repeat in range(args.repeat_time):
-        # load data to cpu
-        # beta = 0.0001 extremely Non-IID, beta = 10000, IID
         split_node_indexes = label_dirichlet_partition(
             labels, len(labels), class_num, args.n_trainer, beta=args.iid_beta
         )
@@ -121,8 +89,8 @@ if __name__ == "__main__":
 
         # determine the resources for each trainer
         @ray.remote(
-            num_gpus=num_gpus,
-            num_cpus=num_cpus,
+            num_gpus=num_gpus_per_client,
+            num_cpus=num_cpus_per_client,
             scheduling_strategy="SPREAD",
         )
         class Trainer(Trainer_General):
@@ -168,13 +136,6 @@ if __name__ == "__main__":
             for i in range(args.n_trainer)
         ]
 
-        args.log_dir = increment_dir(Path(args.logdir) / "exp")
-        os.makedirs(args.log_dir)
-        yaml_file = str(Path(args.log_dir) / "args.yaml")
-        with open(yaml_file, "w") as out:
-            yaml.dump(args.__dict__, out, default_flow_style=False)
-
-        writer = SummaryWriter(args.log_dir)
         # clear cache
         torch.cuda.empty_cache()
         server = Server(
@@ -214,74 +175,8 @@ if __name__ == "__main__":
         for i in range(args.global_rounds):
             server.train(i)
 
-        results = [trainer.get_all_loss_accuray.remote() for trainer in server.trainers]
-        results = np.array([ray.get(result) for result in results])
-
-        client_id = 0
-        for result in results:
-            for iteration in range(len(result[0])):
-                writer.add_scalar(
-                    "Train Loss/Client_{}".format(client_id),
-                    result[0][iteration],
-                    iteration,
-                )
-            for iteration in range(len(result[1])):
-                writer.add_scalar(
-                    "Train Accuracy/Client_{}".format(client_id),
-                    result[1][iteration],
-                    iteration,
-                )
-            for iteration in range(len(result[2])):
-                writer.add_scalar(
-                    "Test Loss/Client_{}.format(client_id)",
-                    result[2][iteration],
-                    iteration,
-                )
-            for iteration in range(len(result[3])):
-                writer.add_scalar(
-                    "Test Accuracy/Client_{}".format(client_id),
-                    result[3][iteration],
-                    iteration,
-                )
-            client_id += 1
-
         train_data_weights = [len(i) for i in in_com_train_node_indexes]
         test_data_weights = [len(i) for i in in_com_test_node_indexes]
-
-        average_train_loss = np.average(
-            [row[0] for row in results], weights=train_data_weights, axis=0
-        )
-        average_train_accuracy = np.average(
-            [row[1] for row in results], weights=train_data_weights, axis=0
-        )
-        average_test_loss = np.average(
-            [row[2] for row in results], weights=test_data_weights, axis=0
-        )
-        average_test_accuracy = np.average(
-            [row[3] for row in results], weights=test_data_weights, axis=0
-        )
-
-        for iteration in range(len(results[0][0])):
-            writer.add_scalar(
-                "Train Loss/Clients_Overall".format(),
-                average_train_loss[iteration],
-                iteration,
-            )
-            writer.add_scalar(
-                "Train Accuracy/Clients_Overall".format(),
-                average_train_accuracy[iteration],
-                iteration,
-            )
-            writer.add_scalar(
-                "Test Loss/Clients_Overall".format(),
-                average_test_loss[iteration],
-                iteration,
-            )
-            writer.add_scalar(
-                "Train Accuracy/Clients_Overall".format(),
-                average_test_accuracy[iteration],
-                iteration,
-            )
 
         results = [trainer.local_test.remote() for trainer in server.trainers]
         results = np.array([ray.get(result) for result in results])
@@ -295,46 +190,4 @@ if __name__ == "__main__":
 
         print(average_final_test_loss, average_final_test_accuracy)
 
-        # sleep(5)  # wait for print message from remote workers
-        filename = (
-            args.dataset
-            + "_"
-            + args.fedtype
-            + "_"
-            + str(args.num_layers)
-            + "_layer_"
-            + str(args.num_hops)
-            + "_hop_iid_beta_"
-            + str(args.iid_beta)
-            + "_n_trainer_"
-            + str(args.n_trainer)
-            + "_local_step_"
-            + str(args.local_step)
-            + ".txt"
-        )
-        with open(filename, "a+") as a:
-            a.write(f"{average_final_test_loss} {average_final_test_accuracy}\n")
-            average_final_test_loss_repeats.append(average_final_test_loss)
-            average_final_test_accuracy_repeats.append(average_final_test_accuracy)
-
-    # finish experiments
-    with open(
-        f"{args.dataset}_{args.fedtype}_{args.num_layers}_layer_{args.num_hops}_hop_iid_beta_{args.iid_beta}_n_trainer_{args.n_trainer}_local_step_{args.local_step}.txt",
-        "a+",
-    ) as a:
-        a.write(
-            f"average_testing_loss {np.average(average_final_test_loss_repeats)} std {np.std(average_final_test_loss_repeats)}\n"
-        )
-        a.write(
-            f"average_testing_accuracy {np.average(average_final_test_accuracy_repeats)} std {np.std(average_final_test_accuracy_repeats)}\n"
-        )
-
-    print(
-        f"average_testing_loss {np.average(average_final_test_loss_repeats)} std {np.std(average_final_test_loss_repeats)}"
-    )
-    print(
-        f"average_testing_accuracy {np.average(average_final_test_accuracy_repeats)} std {np.std(average_final_test_accuracy_repeats)}"
-    )
-
 ray.shutdown()
-
--- docs/index.rst
+++ docs/index.rst
@@ -10,58 +10,40 @@ FedGraph
 
 
 **FedGraph** *(Federated Graph)* is a library built upon
-`PyTorch <https://pytorch.org/>`__ to easily train Graph Neural Networks
-(GNNs) under federated (distributed) setting.
+[PyTorch Geometric](https://pytorch-geometric.readthedocs.io) and
+[Ray](https://docs.ray.io/) to easily train Graph Neural Networks
+under federated or distributed setting.
 
-It supports various federated training methods of graph neural networks
-under simulated and real federated environment and supports
-communication between clients and the central server for model update
-and information aggregation.
 
-Various Training Tasks
+It supports various federated training methods of graph neural networks under simulated and real federated environment and supports communication between clients and the central server for model update and information aggregation.
+
+
+Main Focus
 ----------------------
 
--  **Node classification (and link prediction) on a single large graph
-   (Main focus)**: Our library recoveres information stored in other
-   client without affecting the privacy of users.
+- **Federated Node Classification with Cross-Client Edges**: Our library supports communicating information stored in other client without affecting the privacy of users.
 
--  **Graph classification on multiple graphs**
+- **Federated Graph Classification**: Our library supports federated graph classification with non-IID graphs.
 
-Cross Platform Deployment
+
+Cross Platform Training
 -------------------------
 
--  **Computers (Linux, Mac OS, Windows)**
--  **Edge devices (Raspberry Pi, Jeston Nano)**
--  **Mobile phones (Andriod, iOS)**
+- We support federated training across Linux, macOS and Windows operating systems.
 
 Library Highlights
 ------------------
 
-Whether you are a federated (distributed) learning researcher or
-first-time user of federated (distributed) learning toolkits, here are
-some reasons to try out FedGraph for federated learning on
-graph-structured data.
+Whether you are a federated learning researcher or first-time user of federated learning toolkits, here are some reasons to try out FedGraph for federated learning on graph-structured data.
 
--  **Easy-to-use and unified API**: All it takes is 10-20 lines of code
-   to get started with training a federated GNN model. FedGraph is
-   *PyTorch-on-the-rocks*: It utilizes a tensor-centric API and keeps
-   design principles close to vanilla PyTorch. If you are already
-   familiar with PyTorch, utilizing FedGraph is straightforward.
--  **Comprehensive and well-maintained GNN models**: Most of the
-   state-of-the-art federated graph training methods have been
-   implemented by library developers or authors of research papers and
-   are ready to be applied.
--  **Great flexibility**: Existing FedGraph models can easily be
-   extended for conducting your own research with GNNs. Making
-   modifications to existing models or creating new architectures is
-   simple, thanks to its easy-to-use message passing API, and a variety
-   of operators and utility functions.
--  **Large-scale real-world GNN models**: We focus on the need of FedGNN
-   applications in challenging real-world scenarios, and support
-   learning on diverse types of graphs, including but not limited to:
-   scalable FedGNNs for graphs with millions of nodes; dynamic FedGNNs
-   for node predictions over time; heterogeneous FedGNNs with multiple
-   node types and edge types.
+- **Easy-to-use and unified API**: All it takes is 10-20 lines of code to get started with training a federated GNN model. GNN models are PyTorch models provided by PyG and DGL. The federated training process is handled by Ray. We abstract away the complexity of federated graph training and provide a unified API for training and evaluating FedGraph models.
+
+- **Various FedGraph methods**: Most of the state-of-the-art federated graph training methods have been implemented by library developers or authors of research papers and are ready to be applied.
+
+- **Great flexibility**: Existing FedGraph models can easily be extended for conducting your own research. Simply inherit the base class of trainers and implement your own methods.
+
+- **Large-scale real-world FedGraph Training**: We focus on the need of FedGraph applications in challenging real-world scenarios with privacy preservation, and support learning on large-scale graphs across multiple clients.
+
 
 ----
 
