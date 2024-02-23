0d63f4e20e1ce142e7287db94bd69adabb18401a
Junhao Li <128126552+Lijunhaohz@users.noreply.github.com>
2024-02-23 19:59:42 +0000

    revise format

--- docs/examples/simple_code_example_gc.py
+++ docs/examples/simple_code_example_gc.py
@@ -20,8 +20,8 @@ from fedgraph.federated_methods import GC_Train
 #######################################################################
 # Choose the model and dataset
 # ------------
-model = 'GCFL+'
-dataset = 'PROTEINS'
+model = "GCFL+"
+dataset = "PROTEINS"
 
 #######################################################################
 # Load configuration
@@ -30,11 +30,11 @@ config_file = f"src/configs/config_gc_{model}.yaml"
 with open(config_file, "r") as file:
     config = yaml.safe_load(file)
 
-config['data_group'] = dataset
+config["data_group"] = dataset
 
 #######################################################################
 # Run the designated method
 # ------------
-assert model in ['SelfTrain', 'FedAvg', 'FedProx', 'GCFL', 'GCFL+', 'GCFL+dWs'], \
+assert model in ["SelfTrain", "FedAvg", "FedProx", "GCFL", "GCFL+", "GCFL+dWs"], \
     f"Unknown model: {model}"
 GC_Train(config=config)
