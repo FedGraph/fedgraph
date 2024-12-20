3d4d1186cfd4e6040fbccd3793510d3c36c92a00
Kay (Zekuan) Liu <zliu234@uic.edu>
2024-12-20 16:53:12 -0600

    unittest

--- fedgraph/test/test_func.py
+++ fedgraph/test/test_func.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
 
-import torch
 import unittest
+import torch
 
-from numpy.testing import assert_equal
-from numpy.testing import assert_raises
-from numpy.testing import assert_warns
+# from numpy.testing import assert_equal
+# from numpy.testing import assert_raises
+# from numpy.testing import assert_warns
 
 from fedgraph.train_func import accuracy
 
 
 class TestFunc(unittest.TestCase):
-
     def test_accuracy(self):
         output = torch.tensor([[0.1, 0.9], [0.2, 0.8]])
         labels = torch.tensor([1, 0])
         acc = accuracy(output, labels)
-        assert (acc == .5)
+        assert acc == .5
