5a3de312f0452da0d8842536e0bde2fa5852c8f0
Kay (Zekuan) Liu <zliu234@uic.edu>
2024-12-20 17:12:48 -0600

    unittest

--- fedgraph/test/test_func.py
+++ fedgraph/test/test_func.py
@@ -10,6 +10,7 @@ from fedgraph.train_func import accuracy
 # from numpy.testing import assert_raises
 # from numpy.testing import assert_warns
 
+
 class TestFunc(unittest.TestCase):
     def test_accuracy(self):
         output = torch.tensor([[0.1, 0.9], [0.2, 0.8]])
